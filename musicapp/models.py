ac67f0cfbab83e97fac35bb3240e5ceefde524e9
Ayomide Francis - Akinlolu <fayomide324@gmail.com>
2022-10-08 10:15:47 +0100

    update

--- musicapp/models.py
+++ musicapp/models.py
@@ -24,4 +24,27 @@ class Song(models.Model):
     title = models.TextField(max_length=150, blank=True, verbose_name="Title")
     date_released = models.DateField()
     likes = models.IntegerField(default=0)
-    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE, verbose_name="Artiste ID")
+    create_at = models.DateField(auto_now_add=True)
+
+
+    class Meta:
+            ordering = ['created_at']
+            verbose_name = "Song"
+            verbose_name_plural = "Songs"
+        
+    def __str__(self) -> str:
+            return f"{self.title}"
+
+class Lyric(models.Model):
+    content = models.TextField(verbose_name="Content")
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+    created_at = models.DateField(auto_add_now=True)
+
+    class Meta:
+            ordering = ['created_at']
+            verbose_name = "Lyric"
+            verbose_name_plural = "Lyrics"
+        
+    def __str__(self) -> str:
+            return f"{self.content}"
